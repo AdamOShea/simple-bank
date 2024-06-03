372fe072f74a03480cb23bc3ef81eea0e23a24e4
Adam O'Shea <C20372181@mytudublin.ie>
2024-06-03 18:27:45 +0100

    v0.8 createAccount functionality

--- SimpleBank.py
+++ SimpleBank.py
@@ -7,13 +7,14 @@ from dataclasses import dataclass
 
 @dataclass
 class Customer:
-    def __init__(self, uuid, name, dob, email, pin, accounts):
+    def __init__(self, uuid, name, dob, email, pin, currentAccount, savingsAccount):
         self.uuid = uuid
         self.name = name
         self.dob = dob
         self.email = email
         self.pin = pin
-        self.accounts = accounts
+        self.currentAccount = currentAccount
+        self.savingsAccount = savingsAccount
         
     def __str__(self) -> str:
         return f"Customer: {self.name}\nCustomer ID: {self.uuid}\nDate of Birth: {self.dob}\nEmail: {self.email}"
@@ -25,7 +26,7 @@ class Account:
         self.balance = balance
     
     def __str__(self) -> str:
-        return f"Account Holder: {self.customer.name}\nAccount ID: {self.uuid}\nCurrent Balance: {self.balance}"
+        return f"Account Holder: {self.customer}\nAccount ID: {self.uuid}\nCurrent Balance: {self.balance}"
     
 class SavingsAccount(Account):
     def __init__(self, customer, uuid, balance, withdrawLimit):
@@ -34,13 +35,14 @@ class SavingsAccount(Account):
         
 class CurrentAccount(Account):
     def __init__(self, customer, uuid, balance, creditLimit):
-        super().__init__(customer, uuid, balance, creditLimit)
+        super().__init__(customer, uuid, balance)
         self.creditLimit = creditLimit
             
 
 def fetchCustomer(email, pin):
     cust = Query()
     customerDb = TinyDB('customers.json')
+    
     
     searchedCust = customerDb.get(cust.email.matches(email, flags=re.IGNORECASE)  & (cust.pin.matches(pin)))
     
@@ -60,11 +62,11 @@ def registerCustomer():
     dob = input("Enter Your Date of Birth (DD/MM/YYYY): ")
     email = input("Enter Your Email: ")
     pin = input("Please Enter Your 4 Digit PIN (4 Numbers Only): ")
-    accounts = []
     
-    newCustomer = Customer(str(uuid.uuid1()), name, dob, email, pin, accounts)
+    
+    newCustomer = Customer(str(uuid.uuid1()), name, dob, email, pin, None, None)
     customerDb = TinyDB('customers.json')
-    customerDb.insert({'uuid':newCustomer.uuid,'name':newCustomer.name,'dob':newCustomer.dob,'email':newCustomer.email,'pin':newCustomer.pin,'accounts':newCustomer.accounts})
+    customerDb.insert({'uuid':newCustomer.uuid,'name':newCustomer.name,'dob':newCustomer.dob,'email':newCustomer.email,'pin':newCustomer.pin,'currentAccount':newCustomer.currentAccount,'savingsAccount':newCustomer.savingsAccount})
     
     print("Registering Customer....")
     time.sleep(2)
@@ -81,6 +83,7 @@ def loginCustomer():
     pin = input("Enter Your 4 Digit PIN: ")
     
     cust = fetchCustomer(email, pin)
+    
     if cust == False:
         print("Your details were entered incorrectly, please try again...")
         time.sleep(2)
@@ -90,6 +93,46 @@ def loginCustomer():
         time.sleep(3)
         mainMenu(cust)
         
+def addAccountToCustomer(accountType, cust):
+    
+    accountsDb = TinyDB('accounts.json')
+    customerDb = TinyDB('customers.json')
+    entry = Query()
+    
+    if accountType == 1:
+        newAccount = CurrentAccount(cust.uuid, str(uuid.uuid1()), 0, 500)
+        accountsDb.insert({'type':"Current",'uuid':newAccount.uuid,'customer':newAccount.customer,'balance':newAccount.balance,'creditLimit':newAccount.creditLimit})
+        customerDb.update({'currentAccount':newAccount.uuid}, entry.uuid == cust.uuid)
+        cust.currentAccount = newAccount.uuid
+        
+        os.system('cls')
+        print("New Current Account Created!\n")
+        print(newAccount)
+        time.sleep(3)
+        
+    elif accountType == 2:
+        newAccount = SavingsAccount(cust.uuid, str(uuid.uuid1()), 0, 1)
+        accountsDb.insert({'type':"Savings",'uuid':newAccount.uuid,'customer':newAccount.customer,'balance':newAccount.balance,'withdrawLimit':newAccount.withdrawLimit})
+        customerDb.update({'savingsAccount':newAccount.uuid}, entry.uuid == cust.uuid)
+        cust.savingsAccount = newAccount.uuid
+        
+        os.system('cls')
+        print("New Savings Account Created!\n")
+        print(newAccount)
+        time.sleep(3)
+        
+        
+def checkCurrentAccountEligibility(cust):
+    if not cust.currentAccount:
+        return True
+    else:
+        return False
+    
+def checkSavingsAccountEligibility(cust):
+    if not cust.savingsAccount:
+        return True
+    else:
+        return False
     
     
         
@@ -97,7 +140,36 @@ def viewAccounts(cust):
     pass
 
 def createAccount(cust):
-    pass
+    
+    while True:
+        os.system('cls')
+        print("************ Create a new Account ************\n")
+        print("A Current Account can be used at any time and has a €500 credit limit")
+        print("A Savings Account can only make transfers or withdrawals once per month\n")
+        print("(Please note that you can only hold 1 of each type of account)\n")
+        print("Please select the type of account you want to create\n[1] Current Account\n[2] Savings Account\n\n[x] Cancel")
+        
+        
+        menuOption = input("Enter: ")
+        
+        if menuOption == '1':
+            if checkCurrentAccountEligibility(cust):
+                addAccountToCustomer(1, cust)
+            else:
+                print("You already have a current account!")
+                time.sleep(2)
+        elif menuOption == '2':
+            if checkSavingsAccountEligibility(cust):
+                addAccountToCustomer(2, cust)
+            else:
+                print("You already have a savings account!")
+                time.sleep(2)
+        elif menuOption == 'x' or 'X':
+            break
+        else:
+            print("Invalid input! Please try again...")
+            time.sleep(2)
+    
 
 def deleteAccount(cust):
     pass
